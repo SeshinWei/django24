bc26ebbf31e982fc5492631973169cc4bc0e3d5c
chao <chao@itcast.cn>
2019-03-23 16:04:39 +0800

    利用request.GET获取查询字符串数据

--- request_response/views.py
+++ request_response/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 
@@ -20,3 +21,13 @@ def weather2(request, year, city):
     print(year)
 
     return HttpResponse('weather1')
+
+
+# GET /get_query_params/
+def get_query_params(request):
+    """演示获取url查询字符串数据"""
+    # request.GET 后面大写的GET只是一个请求对象的属性而已,和请求方法无关
+    query_dict = request.GET
+    # query_dict.get()  取单个值
+    # query_dict.getlist() 取某个的多个值,返回列表
+    return HttpResponse('get_query_params')
