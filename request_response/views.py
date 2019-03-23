54d514255a1bd73c905f2f8bd09a2df3c0394204
chao <chao@itcast.cn>
2019-03-23 16:43:04 +0800

    利用request.body获取请求体非表单数据

--- request_response/views.py
+++ request_response/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import json
 
 # Create your views here.
+
+"""Django中的视图函数不需要指定请求方法,只要路由匹配成功都可以调用, 查询字符串无论是get请求还是post请求都可以有,但一般都是get请求带查询字符串"""
 
 
 # /weather/beijing/2018
@@ -44,3 +46,14 @@ def get_form_data(request):
 
     return HttpResponse('get_form_data')
 
+
+# POST /get_json/
+def get_json(request):
+    """演示获取请求体中的非表单数据:json"""
+    # 获取请求体非表单数据用body属性,得到bytes类型的数据
+    json_bytes = request.body
+    json_str = json_bytes.decode()
+    dict = json.loads(json_str)  # 将json字符串转换成json字典或json列表
+    # json.dumps()  # 把字典或列表转换成json字符串
+    print(dict)
+    return HttpResponse('get_json')
