d51adc5f25aa27f09b142c3f8d5439fa565030eb
chao <chao@itcast.cn>
2019-03-24 15:32:26 +0800

    装饰器装类视图

--- classview/views.py
+++ classview/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.utils.decorators import method_decorator
 
 # Create your views here.
 
@@ -8,10 +9,26 @@ from django.http import HttpResponse
 类视图必须继承View 
 类视图中的方法名都必须是请求方法名小写
 """
+
+def my_decorator(view_func):
+    """定义装饰器"""
+    def wrapper(request, *args, **kwargs):
+        print('装饰器被调用了')
+        return view_func(request, *args, **kwargs)
+
+    return wrapper
+
+# @my_decorator
+# def index(request):
+#     return HttpResponse('ok')
+
+
+@method_decorator(my_decorator, name='get')
 class DemoView(View):
     """定义类视图"""
 
-    def get(self, request, city, year):
+    # @my_decorator
+    def get(self, request):
         return HttpResponse('get请求业务逻辑')
 
     def post(self, request):
