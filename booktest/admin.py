de4bd77ef77063510417e1143fe2e0bee2218c28
chao <chao@itcast.cn>
2019-03-26 15:47:42 +0800

    在admin中注册模型

--- booktest/admin.py
+++ booktest/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import BookInfo, HeroInfo
 # Register your models here.
+# 如果相让某个模型在admin站点中进行展示 需要装展示的模型注册到admin站点
+
+admin.site.register(BookInfo)
+admin.site.register(HeroInfo)
